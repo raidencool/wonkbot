c932e25baed40622c986cf72ad2dd2d221eb2725
raidencool <44273162+raidencool@users.noreply.github.com>
2018-10-18 22:08:39 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const commando = require('discord.js-commando');
+
+const bot = new commando.Client();
+
+bot.on("guildMemberAdd", member => {
+    let guild = member.guild;
+    guild.defaultChannel.sendMessage(`Welcome ${member.user} to the Wonky Server!The wonkiest place on Earth!`).catch(console.error);
+
+
+  });
+
+
+bot.registry.registerGroup('random', 'Random');
+
+bot.registry.registerGroup('quotes', 'Quotes');
+
+bot.registry.registerGroup('basic', 'Basic');
+
+bot.registry.registerDefaults();
+
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+
+  bot.user.setActivity("Loot hoovering around in Thedas!");
+
+
+
+});
+
+
+bot.login('NTAyMTQ4NTgzNDYyNzk3MzIx.DqpFDA.ogSuUC0YSPgI7actELn8D4m4XEA');
